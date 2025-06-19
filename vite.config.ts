7a8415311979b58e428c92367acbb17273fe34d7
rafi-s-b-m-00 <142818703+Rafi-Luffy@users.noreply.github.com>
2025-06-19 18:10:43 +0530

    Update vite.config.ts and package.json for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PySan/', // 👈 This is the key change for GitHub Pages
   plugins: [react()],
 })
